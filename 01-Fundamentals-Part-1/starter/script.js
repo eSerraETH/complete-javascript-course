71901ac46cebc288dc3f39a457989a0843bc790a
99Thebigdady <79265687+99Thebigdady@users.noreply.github.com>
2021-09-02 15:41:24 -0400

    End day 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,29 @@
+// Test github
+/*
 console.log(69)
+Write this out
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 69);
+//console.log(typeof "Ely");
+
+javascriptIsFun = "WOO!"
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1999;
+console.log(typeof year);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1999
+*/
